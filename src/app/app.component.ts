0819cc9a60d28d7de06527e9b38a1f0e79659dc9
Jose Gomez <josegomez120@gmail.com>
2017-08-12 14:13:13 -0500

    Two way databiding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +10,7 @@ export class AppComponent {
   title : string;
   name : string;
   heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+   vainas = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
   constructor() {
     this.title = 'Jl Jungle';
     this.name = 'xXxXxX';
